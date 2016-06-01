fd2664034a5911b3b84fc42440eff1cfcfc32006
Maximilian Klein <dasmaxl@gmail.com>
2016-06-01 10:14:59 +0200

    (feature) Stop early if no rule applies anymore.

--- src/api.js
+++ src/api.js
@@ -2,16 +2,22 @@
 import {utils} from '@buggyorg/graphtools'
 import {isGenericFree, genericNodes} from './utils.js'
 import rules from './rules'
+import _ from 'lodash'
 
 function applyGenericRules (graph) {
   var newGraph = utils.clone(graph)
-  rules.forEach((f) => f(newGraph))
-  return newGraph
+  var applied = _.reduce(rules, (acc, f) => f(newGraph) || acc, false)
+  return {graph: newGraph, applied}
 }
 
 export function replaceGenerics (graph) {
   for (var n = 0; n < graph.nodes().length * 20; n++) {
-    graph = applyGenericRules(graph)
+    var res = applyGenericRules(graph)
+    if (res.applied) {
+      graph = res.graph
+    } else {
+      break
+    }
   }
   return graph
 }
