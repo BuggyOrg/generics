1635f92a2e44ecbf1932b9cdcbd6ca9eb6c87861
Maximilian Klein <dasmaxl@gmail.com>
2016-06-02 23:08:58 +0200

    (test) Add test for the selectionsort example.

--- test/generics.js
+++ test/generics.js
@@ -25,5 +25,11 @@ describe('Determine generic types', () => {
     var replaced = replaceGenerics(mapGraph)
     expect(genericNodes(replaced)).to.be.empty
   })
+
+  it('can process the recursive map example', () => {
+    var selGraph = readFixture('fullGraphs/selectionsort.json')
+    var replaced = replaceGenerics(selGraph)
+    expect(genericNodes(replaced)).to.be.empty
+  })
 })
 
